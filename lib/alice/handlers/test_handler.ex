83200089c67362f1ab538d1971284a4e61087f87
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-09 21:26:02 +0530

    Update test_handler.ex

--- lib/alice/handlers/test_handler.ex
+++ lib/alice/handlers/test_handler.ex
@@ -11,3 +11,13 @@ defmodule Alice.Handlers.TestHandler do
 
   @doc "`cmd1`: does some stuff"
   def command1(_conn), do: nil
+
+ # This should not show up in the help text at all
+  @doc false
+  def hidden(_conn), do: nil
+
+  @doc """
+  `cmd2`: does some other stuff
+  also this one is multiline
+  """
+  def command2(_conn), do: nil
