9f9d2f604774a887aa8a0aec66c0b7d34d415668
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-10 20:22:33 +0530

    Update utils.ex

--- lib/alice/handlers/utils.ex
+++ lib/alice/handlers/utils.ex
@@ -8,3 +8,21 @@ defmodule Alice.Handlers.Utils do
   command ~r/\bdebug state\z/i, :debug_state
   command ~r/\bdebug slack\z/i, :debug_slack
   command ~r/\bdebug conn\z/i, :debug_conn
+  
+  @doc "`ping` - responds with signs of life"
+  def ping(conn) do
+    ["PONG!", "Can I help you?", "Yes...I'm still here.", "I'm alive!"]
+    |> random_reply(conn)
+  end
+
+  @doc "`info` - info about Alice and the system"
+  def info(conn) do
+    mem = :erlang.memory()
+    total = "Total memory: #{bytes_to_megabytes(mem[:total])}MB"
+    process = "Allocated to processes: #{bytes_to_megabytes(mem[:processes])}MB"
+
+    conn
+    |> reply("Alice #{alice_version()} - https://github.com/alice-bot")
+    |> reply("#{total} - #{process}")
+  end
+  
