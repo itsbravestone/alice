332ee7de5baa26a5e6359eb706ea7799af21346c
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-12 21:36:05 +0530

    Update memory.ex

--- lib/alice/state_backends/memory.ex
+++ lib/alice/state_backends/memory.ex
@@ -7,3 +7,11 @@ defmodule Alice.StateBackends.Memory do
     @fallback_to_any true
     def convert(value)
   end
+
+defimpl MemoryValue, for: Date do
+    def convert(value), do: to_string(value)
+  end
+
+  defimpl MemoryValue, for: Any do
+    def convert(value), do: value
+  end
