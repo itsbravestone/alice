fba97751c0d64e330f69a7d1a87c08400986bbb5
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-17 23:41:24 +0530

    Update redis.ex

--- lib/alice/state_backends/redis.ex
+++ lib/alice/state_backends/redis.ex
@@ -15,3 +15,16 @@ defmodule Alice.StateBackends.Redis do
       _ -> :ok
     end
   end
+
+defp migrate_redis! do
+    state = do_get_state()
+    Enum.each(state, fn {key, val} -> put(state, key, val) end)
+    RedixPool.command!(["SET", @migration_key, true])
+    :ok
+  end
+
+  def get(_state, key, default \\ nil) do
+    ["GET", encode_key(key)]
+    |> RedixPool.command!()
+    |> handle_get_result(default)
+  end
