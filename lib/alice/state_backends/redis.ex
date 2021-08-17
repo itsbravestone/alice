12ca54616599a5d895aba74af01d1668c61b1ff5
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-17 23:41:02 +0530

    Update redis.ex

--- lib/alice/state_backends/redis.ex
+++ lib/alice/state_backends/redis.ex
@@ -4,3 +4,14 @@ defmodule Alice.StateBackends.Redis do
   @behaviour Alice.StateBackends.StateBackend
 
   alias Alice.StateBackends.RedixPool
+
+ @migration_key "Alice.State.Migrated.v0.3.5"
+
+  defp migrate_redis do
+    ["GET", @migration_key]
+    |> RedixPool.command!()
+    |> case do
+      nil -> migrate_redis!()
+      _ -> :ok
+    end
+  end
